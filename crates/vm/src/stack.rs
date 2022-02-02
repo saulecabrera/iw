e8d59945e4f68f25a5f91de6e40562f9ff5dbcc1
Saúl Cabrera <saulecabrera@gmail.com>
2022-02-01 21:18:09 -0500

    feat: Initial, simple implementation of Stack, StackValue and Value

--- crates/vm/src/stack.rs
+++ crates/vm/src/stack.rs
@@ -1,3 +1,28 @@
-pub struct Stack {
+use crate::val::Value ;
 
+pub enum StackValue {
+    Value(Value),
+    Label,
+    Activation
+}
+
+pub enum StackValueType {
+    Value,
+    Label,
+    Activation
+}
+
+impl StackValue {
+    pub fn ty(&self) -> StackValueType {
+        type SVT = StackValueType;
+        match *self {
+            StackValue::Value(_) => SVT::Value,
+            StackValue::Label => SVT::Label,
+            StackValue::Activation => SVT::Activation
+        }
+    }
+}
+
+
+pub struct Stack {
 }
--- crates/vm/src/val.rs
+++ crates/vm/src/val.rs
@@ -1,15 +1,33 @@
+
+pub enum ValueType {
+    I32,
+    I64,
+    F32,
+    F64
+}
+
 pub enum Value {
     // -- Numeric
     I32(i32),
     I64(i64),
-    F32(F32),
-    F64(F64),
+    F32(u32),
+    F64(u64),
 
+    // TODO
     // -- Reference
-    NullRef,
-    FuncRef,
-    ExternRef,
+    // NullRef,
+    // FuncRef,
+    // ExternRef,
 }
 
-pub struct F32(u32);
-pub struct F64(u64);
+impl Value {
+    pub fn ty(&self) -> ValueType 
+    {
+        match *self {
+            Value::I32(_) => ValueType::I32,
+            Value::I64(_) => ValueType::I64,
+            Value::F32(_) => ValueType::F32,
+            Value::F64(_) => ValueType::F64
+        }
+    }
+}
