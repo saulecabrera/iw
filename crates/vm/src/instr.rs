598f9fa7e16d27bf398c7b26e6119307c4333985
Saúl Cabrera <saulecabrera@gmail.com>
2022-03-30 16:11:22 -0400

    feat: Add functionality to retrieve a const

--- crates/vm/src/instr.rs
+++ crates/vm/src/instr.rs
@@ -1,1 +1,24 @@
-pub enum Instr {}
+use crate::val::Value;
+use anyhow::Result;
+use wasmparser::Operator;
+
+pub struct Instr<'a> {
+    kind: Operator<'a>,
+}
+
+// TODO: Track the offset if it's ever needed
+impl<'a> Instr<'a> {
+    pub fn new(kind: Operator<'a>) -> Self {
+        Instr { kind }
+    }
+
+    pub fn eval_const(&self) -> Result<Value> {
+        match self.kind {
+            Operator::I32Const { value } => Ok(Value::I32(value)),
+            Operator::I64Const { value } => Ok(Value::I64(value)),
+            Operator::F32Const { value } => Ok(Value::F32(value.bits())),
+            Operator::F64Const { value } => Ok(Value::F64(value.bits())),
+            _ => anyhow::bail!("Unsupported const instruction {:?}", self.kind),
+        }
+    }
+}
