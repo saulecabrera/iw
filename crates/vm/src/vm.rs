0e2024670ab489bae6c8f70284d3f2734c41b303
Saúl Cabrera <saulecabrera@gmail.com>
2022-03-30 16:11:58 -0400

    feat: Add functionality to eval const expressions

--- crates/vm/src/vm.rs
+++ crates/vm/src/vm.rs
@@ -1,4 +1,6 @@
-use crate::{frame::Frame, label::Label, stack::Stack, val::Value};
+use crate::{frame::Frame, instr::Instr, label::Label, stack::Stack, val::Value};
+use anyhow::Result;
+use wasmparser::InitExpr;
 
 pub struct VM {
     ip: usize,
@@ -9,4 +11,11 @@ pub enum StackEntry {
     Val(Value),
     CallFrame(Frame),
     Label(Label),
+}
+
+pub fn eval_const_expr(expr: &InitExpr) -> Result<Value> {
+    let mut ops_reader = expr.get_operators_reader();
+    let op = ops_reader.read()?;
+    let instr = Instr::new(op);
+    instr.eval_const()
 }
