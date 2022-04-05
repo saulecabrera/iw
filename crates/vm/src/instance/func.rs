7d5427e353d2679a1ffb3a600537537623a60e1d
Saúl Cabrera <saulecabrera@gmail.com>
2022-04-05 19:44:01 -0400

    feat: Finish allocating functions

--- crates/vm/src/instance/func.rs
+++ crates/vm/src/instance/func.rs
@@ -4,12 +4,31 @@ use wasmparser::{FuncType, LocalsReader, OperatorsReader, Type};
 
 pub struct Func<'a> {
     ty: FuncType,
-    locals: Vec<(u32, Type)>,
+    locals: Vec<Type>,
     body: Vec<Instr<'a>>,
 }
 
 impl<'a> Func<'a> {
-    pub fn new(ty: FuncType, locals: LocalsReader, body: OperatorsReader) -> Result<Self> {
-        todo!()
+    pub fn new(ty: FuncType, locals: LocalsReader, body: OperatorsReader<'a>) -> Result<Self> {
+        Ok(Self {
+            ty,
+            locals: locals
+                .into_iter()
+                .try_fold(Vec::new(), |mut acc, local| -> Result<_> {
+                    let (count, ty) = local?;
+                    // NB
+                    // wasmparser returns the locals as (count, type) in order of appearance;
+                    // here we "flatten" the locals into a vector of types
+                    acc.extend_from_slice(&[ty].repeat(count as usize));
+                    Ok(acc)
+                })?,
+
+            body: body
+                .into_iter()
+                .try_fold(Vec::new(), |mut acc, op| -> Result<_> {
+                    acc.push(Instr::new(op?));
+                    Ok(acc)
+                })?,
+        })
     }
 }
