e3e7776451bf1a1c8251ad36db64ea3f4d2ff2ec
Saúl Cabrera <saulecabrera@gmail.com>
2022-04-06 16:23:18 -0400

    refactor: Move allocation process from the instance to the store

--- crates/vm/src/instance/mod.rs
+++ crates/vm/src/instance/mod.rs
@@ -3,12 +3,7 @@ pub mod global;
 pub use func::*;
 pub use global::*;
 
-use crate::{module::Module, store::Store, vm};
-use anyhow::Result;
-use func::Func as FuncInstance;
-use global::Global as GlobalInstance;
-use std::borrow::BorrowMut;
-use wasmparser::{FuncType, Global as GlobalReader};
+use wasmparser::FuncType;
 
 pub type Index = usize;
 // TODO(@saulecabrera): Add imports once extern values are supported
@@ -19,49 +14,10 @@ pub struct Instance {
 }
 
 impl Instance {
-    pub(crate) fn new(store: &mut Store, module: &Module, store_index: Index) -> Result<Self> {
-        let types = module.func_types();
-
-        Self::allocate(store.borrow_mut(), module, store_index)?;
-
-        Ok(Instance {
+    pub(crate) fn new(types: Vec<FuncType>, store_index: Index) -> Self {
+        Instance {
             index: store_index,
             types,
-        })
-    }
-
-    fn allocate(store: &mut Store, module: &Module, index: Index) -> Result<()> {
-        Self::allocate_globals(store.borrow_mut(), &module.globals, index)?;
-        Self::allocate_funcs(store.borrow_mut(), &module, index)
-    }
-
-    fn allocate_globals(
-        store: &mut Store,
-        globals: &Vec<GlobalReader>,
-        index: Index,
-    ) -> Result<()> {
-        globals.iter().try_for_each(|global| {
-            let value = vm::eval_const_expr(&global.init_expr)?;
-            store.allocate_global(index, GlobalInstance::new(value, global.ty))
-        })
-    }
-
-    fn allocate_funcs(store: &mut Store, module: &Module, index: Index) -> Result<()> {
-        let functions = &module.functions;
-        let codes = &module.codes;
-        let types = &module.func_types();
-
-        codes
-            .iter()
-            .zip(functions)
-            .try_for_each(|(body, func_index)| {
-                let locals = body.get_locals_reader()?;
-                let ops = body.get_operators_reader()?;
-                let ty = types
-                    .get(*func_index as usize)
-                    .ok_or(anyhow::anyhow!("Invalid func index {}", func_index))?;
-
-                store.allocate_func(index, FuncInstance::new(ty.clone(), locals, ops)?)
-            })
+        }
     }
 }
--- crates/vm/src/store.rs
+++ crates/vm/src/store.rs
@@ -1,11 +1,10 @@
 use crate::addressable::Addressable;
-use crate::instance::Func;
-use crate::instance::Global;
-use crate::instance::{Index as InstanceIndex, Instance};
+use crate::instance::{Index as InstanceIndex, Instance, global::Global, func::Func};
 use crate::module::Module;
+use crate::vm;
 use anyhow::{bail, Result};
-use std::borrow::BorrowMut;
 use std::collections::HashMap;
+use wasmparser::Global as GlobalReader;
 
 #[derive(Default)]
 pub struct Store<'a> {
@@ -20,7 +19,7 @@ impl<'a> Store<'a> {
         Self::default()
     }
 
-    pub fn instantiate(&mut self, module: &Module, name: Option<String>) -> Result<Instance> {
+    pub fn instantiate(&mut self, module: &'a Module, name: Option<String>) -> Result<Instance> {
         let index = self.instances.len();
 
         if let Some(name) = &name {
@@ -30,19 +29,49 @@ impl<'a> Store<'a> {
             self.instances_env.insert(name.to_owned(), index);
         }
 
-        let instance = Instance::new(self.borrow_mut(), module, index)?;
+        self.allocate(module, index)?;
+
+        let instance = Instance::new(module.func_types(), index);
         self.instances.push(instance.clone());
 
         Ok(instance)
     }
 
-    pub fn allocate_global(&mut self, instance_index: InstanceIndex, global: Global) -> Result<()> {
-        self.globals.push(instance_index, global);
-        Ok(())
+    fn allocate(&mut self, module: &'a Module, index: InstanceIndex) -> Result<()> {
+        self.allocate_globals(&module.globals, index)?;
+        self.allocate_funcs(module, index)
     }
 
-    pub fn allocate_func(&mut self, index: InstanceIndex, func: Func<'a>) -> Result<()> {
-        self.funcs.push(index, func);
-        Ok(())
+    fn allocate_globals(
+        &mut self,
+        globals: &Vec<GlobalReader>,
+        index: InstanceIndex,
+    ) -> Result<()> {
+        globals.iter().try_for_each(|global| {
+            let value = vm::eval_const_expr(&global.init_expr)?;
+            self.globals.push(index, Global::new(value, global.ty));
+            Ok(())
+        })
+    }
+
+
+    fn allocate_funcs(&mut self, module: &'a Module, index: InstanceIndex) -> Result<()> {
+        let functions = &module.functions;
+        let codes = &module.codes;
+        let types = &module.func_types();
+
+        codes
+            .iter()
+            .zip(functions)
+            .try_for_each(|(body, func_index)| {
+                let locals = body.get_locals_reader()?;
+                let ops = body.get_operators_reader()?;
+                let ty = types
+                    .get(*func_index as usize)
+                    .ok_or(anyhow::anyhow!("Invalid func index {}", func_index))?;
+
+                self.funcs.push(index, Func::new(ty.clone(), locals, ops)?);
+                Ok(())
+            })
     }
 }
