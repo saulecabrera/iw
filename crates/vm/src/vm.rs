1278e9c1cb9d6e855ddb6f07ba1b618d7758be75
Saúl Cabrera <saulecabrera@gmail.com>
2022-06-02 16:19:15 -0400

    feat: Add a way to resolve index in a funcref

--- crates/vm/src/vm.rs
+++ crates/vm/src/vm.rs
@@ -1,4 +1,12 @@
-use crate::{frame::Frame, instr::Instr, label::Label, stack::Stack, val::Value};
+use crate::{
+    addressable::{Addr, Slot, Slottable},
+    frame::Frame,
+    instance::{Func, Index},
+    instr::Instr,
+    label::Label,
+    stack::Stack,
+    val::{RefValue, Value},
+};
 use anyhow::{Context, Result};
 use wasmparser::InitExpr;
 
@@ -21,4 +29,18 @@ pub fn resolve_constant_expr(expr: &InitExpr) -> Result<Value> {
     instr
         .const_value()
         .with_context(|| format!("{:?} is not a constant instruction", instr))
+}
+
+/// Resolves a funcref initializer expr
+pub fn resolve_funcref_expr(expr: &InitExpr, instance_index: Index) -> Result<RefValue> {
+    let mut ops_reader = expr.get_operators_reader();
+    let op = ops_reader.read()?;
+    let instr = Instr::try_from(op)?;
+    let func_idx = instr
+        .funcref_idx()
+        .with_context(|| format!("{:?} is not a function reference", instr))?;
+
+    let addr = Addr::new_unsafe(instance_index, func_idx, Func::slot());
+
+    Ok(RefValue::FuncRef(addr))
 }
